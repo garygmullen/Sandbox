4335886cfaf8f311767657b7fda07564eb95e3a8
Gary Mullen <garygmullen@gmail.com>
2013-05-25 16:56:18 -0700

    Added returnNumber

--- mytest.c
+++ mytest.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
+int returnNumber( 
+	int num
+	)
+{
+	return num;
+}
+
 int main(
     int argc,
     char **argv
     )
 {
 	printf ("simple test app\n" );
+	printf ("got %d\n", returnNumber( 5 ) );
     return ;
 }
